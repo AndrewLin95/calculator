ef4ac2288e51d53b899ad08f2428095fe717e2ac
AndrewLin95 <andrew.lin88@hotmail.com>
2022-06-27 21:46:51 -0400

    simplified code. added functionality for negative/positive value changes

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 const numberClusterPress = document.querySelectorAll('.btnNum');
 const numberOperandPress = document.querySelectorAll('.btnFunc');
+const numberModifyPress = document.querySelectorAll('.btnModify');
 const bottomBox = document.querySelector('#calculatorOutputBox');
 const topBox = document.querySelector('#calculatorCalculateBox');
 
@@ -7,61 +8,67 @@ let currentInputValue = '';
 let currentOutputValue = '';
 let memoryValue = '';
 let memoryOperand = '';
+let newValue = '';
 
 let initialValueCheck = false;
 let initialOperandCheck = false;
 let equalCheck = false;
+let newInputCheck = false;                                                  // if a number is pressed after an equation is completed, a brandnew input is created.
 
 /*  Outputs the string of the values inputted to the bottom text display and stores the value in currentOutputValue */
 
 const displayOutputValue = () => {
     currentOutputValue += currentInputValue;
-    console.log(currentOutputValue);
     bottomBox.textContent = currentOutputValue;
 }
  
 const additionFunction = () => {                                                           
-    let newValue = eval(memoryValue + memoryOperand + currentOutputValue);  // stores the evaluation of the memory value and the new value with the previous memory operand (which is addition)        
-    memoryOperand = '+';                                                    // adds the addition operand to memory which wil be used to calculate the value whenever any other operand is pressed. ADDED AFTER calculations are made.
-    memoryValue = newValue;                                                 // saves the calculated value to be used in furthur calculations
-    topBox.textContent = newValue + ' +';                                   // below code displays the initial steps for more actions
-    bottomBox.textContent = newValue;
-    currentOutputValue = '';
+    if (initialValueCheck == true){
+        newValue = eval(memoryValue + memoryOperand + currentOutputValue);      // stores the evaluation of the memory value and the new value with the previous memory operand (which is addition)        
+        memoryOperand = '+';                                                    // adds the addition operand to memory which wil be used to calculate the value whenever any other operand is pressed. ADDED AFTER calculations are made.
+        displayFunctionChange();
+    }
 }
 
 const subtractionFunction = () => {
-    let newValue = eval(memoryValue + memoryOperand + currentOutputValue);
-    memoryOperand = '-';  
-    memoryValue = newValue;                                                 
-    topBox.textContent = newValue + ' -';                                  
-    bottomBox.textContent = newValue;
-    currentOutputValue = '';
+    if (initialValueCheck == true){
+        newValue = eval(memoryValue + memoryOperand + currentOutputValue);
+        memoryOperand = '-';  
+        displayFunctionChange();
+    }
 }
 
 const multiplyFunction = () => {
-    let newValue = eval(memoryValue + memoryOperand + currentOutputValue);
-    memoryOperand = '*';  
-    memoryValue = newValue;                                                 
-    topBox.textContent = newValue + ' *';                                  
-    bottomBox.textContent = newValue;
-    currentOutputValue = '';
+    if (initialValueCheck == true){
+        newValue = eval(memoryValue + memoryOperand + currentOutputValue);
+        memoryOperand = '*';  
+        displayFunctionChange();
+    }
 }
 
 const divideFunction = () => {
-    let newValue = eval(memoryValue + memoryOperand + currentOutputValue);
-    memoryOperand = '/';  
-    memoryValue = newValue;                                                 
-    topBox.textContent = newValue + ' /';                                  
+    if (initialValueCheck == true){
+        newValue = eval(memoryValue + memoryOperand + currentOutputValue);
+        memoryOperand = '/';  
+        displayFunctionChange();
+    }
+}
+
+const displayFunctionChange = () => {
+    memoryValue = newValue;                                                         // saves the calculated value to be used in furthur calculations
+    topBox.textContent = newValue + ' ' + memoryOperand;                                  // below code displays the initial steps for more actions
     bottomBox.textContent = newValue;
     currentOutputValue = '';
 }
 
 const equalFunction = () => {
     let newValue = eval(memoryValue + memoryOperand + currentOutputValue);
+    topBox.textContent = memoryValue + ' ' + memoryOperand + ' ' + currentOutputValue + " ="
     memoryValue = newValue;                                                     // lets the resulting value be used to start a new value
-    memoryOperand = '';                                                         // resets operand memory because no new calculations are made.
-    currentOutputValue = '';                                                    
+    memoryOperand = '';                                                         // resets operand memory because no new calculations are made.                                                   
+    currentOutputValue = '';
     equalCheck = false;                                                         // prevents equal from being pressed again until another operand is pressed
+    newInputCheck = true;
     bottomBox.textContent = newValue;                              
 }
 
@@ -70,12 +77,16 @@ const equalFunction = () => {
 
 numberClusterPress.forEach(numberButtonPress => {                                  
     numberButtonPress.addEventListener('click', () => {
-        if (numberButtonPress.className == 'waves-effect blue-grey lighten-5 btn-flat btnCluster btnNum'){
+        if (numberButtonPress.className == 'waves-effect blue-grey lighten-5 btn-flat btnCluster btnNum' && newInputCheck == false){
             currentInputValue = numberButtonPress.id;
             initialValueCheck = true;
             displayOutputValue();
-        } else {
-            return;
+        } else if (numberButtonPress.className == 'waves-effect blue-grey lighten-5 btn-flat btnCluster btnNum' && newInputCheck == true) {
+            memoryValue = '';                                                           // resets memory so value cannot be added to it
+            topBox.textContent = '';                                                    // resets the top box
+            currentInputValue = numberButtonPress.id;
+            initialValueCheck = true;
+            displayOutputValue();
         }
     })
 })
@@ -84,6 +95,8 @@ numberClusterPress.forEach(numberButtonPress => {
 
 numberOperandPress.forEach(numberFuncPress => {
     numberFuncPress.addEventListener('click', () => {
+        newInputCheck = false;
+        equalCheck = true;
         if (initialValueCheck == true && initialOperandCheck == false){     
             initialOperandCheck = true;                                     // Sets the initialOperandCheck to true which allows for the specific function buttons to be operable
             memoryValue = currentOutputValue;                               // the below code sets the inital displays to show the general numbers                     
@@ -93,26 +106,31 @@ numberOperandPress.forEach(numberFuncPress => {
 
         if (numberFuncPress.id == 'add' && initialOperandCheck == true){    // triggers when the add button is specifically pressed
             topBox.textContent = memoryValue + " +";                        // adds " +" to the top display box to signify which operand has been pressed
-            equalCheck = true;
             additionFunction();
         } else if (numberFuncPress.id == 'subtract' && initialOperandCheck == true){
             topBox.textContent = memoryValue + " -"; 
-            equalCheck = true;
             subtractionFunction();
         } else if (numberFuncPress.id == 'multiply' && initialOperandCheck == true){
             topBox.textContent = memoryValue + " *"; 
-            equalCheck = true;
             multiplyFunction();
         } else if (numberFuncPress.id == 'divide' && initialOperandCheck == true){
-            topBox.textContent = memoryValue + " +";
-            equalCheck = true;
+            topBox.textContent = memoryValue + " /";
             divideFunction();
-        } else if (numberFuncPress.id == 'equal' && initialOperandCheck == true && equalCheck == true){
-            topBox.textContent = memoryValue + ' ' + memoryOperand + ' ' + currentOutputValue + " ="
+        } else if (numberFuncPress.id == 'equal' && initialOperandCheck == true){
             equalFunction();
         } else {
             return;
         }
+        initialValueCheck = false;
     })
 })
 
+
+numberModifyPress.forEach(numModifyPress => {
+    numModifyPress.addEventListener('click', () => {
+        if (numModifyPress.id == 'plusMinus' && initialValueCheck == true){
+            currentOutputValue = "-" + currentOutputValue;
+            bottomBox.textContent = currentOutputValue;
+        }
+    })
+})
