5d7cc0e30e3c348a8166bdb51a4a9606cb2a4dac
AndrewLin95 <andrew.lin88@hotmail.com>
2022-06-28 18:49:26 -0400

    general functionality programmed

--- javascript.js
+++ javascript.js
@@ -29,7 +29,6 @@ const operate = () => {
             results = parseInt(calcArray[0]) / parseInt(displayString);
             break; 
     } 
-
 }
 
 numInput.forEach(buttonPress => {
@@ -39,13 +38,23 @@ numInput.forEach(buttonPress => {
     })
 })
 
-calculationInput.forEach(calculatePress => {
+calculationInput.forEach(calculatePress => {                                            // in all of the IF statements, it checks if lastElement != undefined; can I simplify that
     calculatePress.addEventListener('click', () => {
         lastElement = calcArray[1];
-        if (displayString != '' && lastElement === undefined){                        // if a value was pressed and no opeartor was pressed. LastElement = peek at array to check if operator was stored.
+        if (displayString && lastElement === undefined){                        // if a value was pressed and no opeartor was pressed. LastElement = peek at array to check if operator was stored.
             calcArray.push(displayString);
             calcArray.push(calculatePress.id);
             topTextBox.textContent = `${calcArray[0]} ${calculatePress.id}`;
+        } else if (!displayString && lastElement != undefined){               // if no imputs were pressed and the operation changes, update the array with the new operator
+            calcArray.pop();
+            calcArray.push(calculatePress.id);
+            topTextBox.textContent = `${calcArray[0]} ${calculatePress.id}`;
+        } else if (calculatePress.id === '=' && lastElement != undefined){          // if enter is pressed and there is no operator pressed
+            operate();
+            bottomTextBox.textContent = results;
+            topTextBox.textContent = `${calcArray[0]} ${calcArray[1]} ${displayString} ${calculatePress.id}`;
+            calcArray = [];
+            calcArray.push(results);
         } else {
             operate();
             bottomTextBox.textContent = results;
@@ -55,51 +64,5 @@ calculationInput.forEach(calculatePress => {
             calcArray.push(calculatePress.id);
         }
         displayString = '';
-
-
     })
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
