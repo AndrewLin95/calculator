76efc5bed7be4a71cb861b7e0899d69b581a75ab
AndrewLin95 <andrew.lin88@hotmail.com>
2022-06-28 21:26:20 -0400

    added functionality for func buttons except for equal

--- javascript.js
+++ javascript.js
@@ -1,68 +1,70 @@
 const topTextBox = document.querySelector('#calculatorCalculateBox');
 const bottomTextBox = document.querySelector('#calculatorOutputBox');
 const numInput = document.querySelectorAll('.btnNum');
-const calculationInput = document.querySelectorAll('.btnFunc');
+const funcInput = document.querySelectorAll('.btnFunc');
+const modifyInput = document.querySelectorAll('.btnModify');
 
-let calcArray = [];
-let lastElement = '';
-let displayString = '';
-let currentValue = '';
-let results = 0;
+let currentValue = '';              // if empty = will return false, if filled, will return true
+let valueOne = '';
+let valueTwo = '';
+let operator = '';
+let operator2 = '';
 
-const bottomDisplayFunc = () => {
-    displayString += currentValue;
-    bottomTextBox.textContent = displayString;
-}
-
-const operate = () => {
-    switch (lastElement) {
-        case '+':
-            results = parseInt(calcArray[0]) + parseInt(displayString);
-            break;
-        case '-':
-            results = parseInt(calcArray[0]) - parseInt(displayString);
-            break;
-        case '*':
-            results = parseInt(calcArray[0]) * parseInt(displayString);
-            break;   
-        case '/':
-            results = parseInt(calcArray[0]) / parseInt(displayString);
-            break; 
-    } 
+const bottomDisplayFunc = (value) => {
+    currentValue += value;
+    bottomTextBox.textContent = currentValue;
 }
 
 numInput.forEach(buttonPress => {
     buttonPress.addEventListener('click', () => {
-        currentValue = buttonPress.id;
-        bottomDisplayFunc();
+        bottomDisplayFunc(buttonPress.id);
     })
 })
 
-calculationInput.forEach(calculatePress => {                                            // in all of the IF statements, it checks if lastElement != undefined; can I simplify that
-    calculatePress.addEventListener('click', () => {
-        lastElement = calcArray[1];
-        if (displayString && lastElement === undefined){                        // if a value was pressed and no opeartor was pressed. LastElement = peek at array to check if operator was stored.
-            calcArray.push(displayString);
-            calcArray.push(calculatePress.id);
-            topTextBox.textContent = `${calcArray[0]} ${calculatePress.id}`;
-        } else if (!displayString && lastElement != undefined){               // if no imputs were pressed and the operation changes, update the array with the new operator
-            calcArray.pop();
-            calcArray.push(calculatePress.id);
-            topTextBox.textContent = `${calcArray[0]} ${calculatePress.id}`;
-        } else if (calculatePress.id === '=' && lastElement != undefined){          // if enter is pressed and there is no operator pressed
-            operate();
-            bottomTextBox.textContent = results;
-            topTextBox.textContent = `${calcArray[0]} ${calcArray[1]} ${displayString} ${calculatePress.id}`;
-            calcArray = [];
-            calcArray.push(results);
-        } else {
-            operate();
-            bottomTextBox.textContent = results;
-            topTextBox.textContent = `${results} ${calculatePress.id}`;
-            calcArray = [];
-            calcArray.push(results);
-            calcArray.push(calculatePress.id);
+funcInput.forEach(funcPress => {
+    funcPress.addEventListener('click', () => {
+        if (!operator && currentValue){
+            valueOne = currentValue;
+            operator = funcPress.id;
+            topTextBox.textContent = `${valueOne} ${operator}`;
+            currentValue = '';
+        } else if (valueOne && currentValue){
+            valueTwo = currentValue;
+            operate(operator, valueOne, valueTwo);
+            operator = funcPress.id;
+            topTextBox.textContent = `${valueOne} ${operator}`;
+            bottomTextBox.textContent = valueOne;
+            currentValue = '';
+        } else if (valueOne && !currentValue){
+            operator = funcPress.id;
+            topTextBox.textContent = `${valueOne} ${operator}`;
         }
-        displayString = '';
     })
 })
+
+
+function operate(operator, a, b){
+    let aVal = parseInt(a);
+    let bVal = parseInt(b);
+
+    switch (operator){
+        case "+":
+            valueOne = aVal + bVal;
+            break;
+        case "-":
+            valueOne = aVal - bVal;
+            break;
+        case '/':
+            if (b === 0){
+                bottomTextBox.textContent = "Error!";   
+                //clear function    
+                return;
+            } 
+            valueOne = aVal / bVal;
+            break;
+        case '*':
+            valueOne = aVal * bVal;
+            break;
+    }
+
+}
